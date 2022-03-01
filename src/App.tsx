80c7c71f6e9bd591575fbc2bf858bb6ea40e64db
Christina Song <christina.song202@gmail.com>
2022-03-01 01:51:51 -0500

    Button component, added sizes scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Button } from "./components/index";
 import "./App.scss";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <Button text={"hello world"} color={"purple-text"} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,1 +1,21 @@
 import React from "react";
+import "./Button.scss";
+
+type ButtonClass = "purple" | "cyan" | "purple-text";
+
+type ButtonProps = {
+  text: string;
+  color: ButtonClass;
+};
+
+const Button = (props: ButtonProps) => {
+  const { text, color } = props;
+
+  return (
+    <button type="button" className={color}>
+      {text}
+    </button>
+  );
+};
+
+export { Button };
